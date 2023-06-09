0d15921cc66d68beee34f84d7c5a65516b8e8ac0
Wandati <Petersimon801@gmail.com>
2023-06-09 19:44:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id.append("victory")
+const element=document.getElementById("victory")
+element.textContent ="Marvin is the champion";
